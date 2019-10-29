4c3a9506438f3f976ac81e67c3e2ac6036ad3823
ErwannLG <e.legall@codeur.online>
2019-10-29 15:28:29 +0100

    adding user position on map

--- Components/Map.js
+++ Components/Map.js
@@ -9,16 +9,48 @@ import { Ionicons as Icon } from '@expo/vector-icons';
 import { DrawerNavigator, DrawerItems, Navigation } from 'react-navigation';
 import { Toolbar } from 'react-native-material-ui';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions'
+
 export default class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       buttonSearchPress: false,
-      buttonLeftPressColor: "white"
+      buttonLeftPressColor: "white",
+      location : null,
+      region: null,
+      errorMessage: null,
     };
   }
   static navigationOptions = {
     drawerLabel: 'Map',
+  };
+
+  componentWillMount() {
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    const { latitude, longitude } = location.coords;
+
+    const region = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    }
+
+    this.setState({ location });
   };
 
   render() {
@@ -26,35 +58,41 @@ export default class Map extends React.Component {
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
         <Toolbar
-          navigation={this.props.navigation}
-          leftElement="menu"
-          onLeftElementPress={ () => {this.props.navigation.toggleDrawer()}}
-          centerElement="NeversNow"
-          style={{
-              container: { backgroundColor: '#302743' },
-              leftElement: { color: this.state.buttonLeftPressColor },
-              titleText: { color: 'white' },
-              rightElement: { color: 'white' }
+        navigation={this.props.navigation}
+        leftElement="menu"
+
+        onLeftElementPress={ () => {this.props.navigation.toggleDrawer()}}
+        centerElement="NeversNow"
+        style={{
+            container: { backgroundColor: '#302743' },
+            leftElement: { color: this.state.buttonLeftPressColor },
+            titleText: { color: 'white' },
+            rightElement: { color: 'white' }
           }}
-          searchable={{
-            autoFocus: true,
-            placeholder: 'Rechercher',
-            onSearchClosed: () => {if (this.state.buttonSearchPress == true){
-              this.setState({
-                buttonLeftPressColor: "white",
-                buttonSearchPress: false,
-              });
+        searchable={{
+          autoFocus: true,
+          placeholder: 'Rechercher',
+          onSearchClosed: () => {if (this.state.buttonSearchPress == true){
+            this.setState({
+              buttonLeftPressColor: "white",
+              buttonSearchPress: false,
+            });
             }},
-            onSearchPressed: () => {if(this.state.buttonSearchPress == false){
-              this.setState({
-                buttonSearchPress: true,
-                buttonLeftPressColor: "#302743"
-              });
-            }}
-          }}
+          onSearchPressed: () => {if(this.state.buttonSearchPress == false){
+            this.setState({
+              buttonSearchPress: true,
+              buttonLeftPressColor: "#302743"
+            });
+          }
+        }
+      }
+        }
+
         />
         <View style={styles.header}>
-          <MapView style={styles.mapStyle}
+          <MapView
+            style={styles.mapStyle}
+            showsUserLocation
             initialRegion={{
               latitude: 46.9896,
               longitude: 3.159,
